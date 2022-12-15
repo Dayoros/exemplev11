4201a1c1c8ab211a363676c7845118a057e4a9dc
Nico <nd.news.perso@gmail.com>
2022-12-15 15:56:54 +0100

    Ajout final

--- src/main/java/com/example/exemplev11/Exemplev11Application.java
+++ src/main/java/com/example/exemplev11/Exemplev11Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Exemplev11Application {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(Exemplev11Application.class, args);
     }
 
