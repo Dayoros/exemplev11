00718de01805e980f4dcca4ed10c57c002f85a7a
omnistrain <105783100+omnistrain@users.noreply.github.com>
2022-12-15 15:04:57 +0100

    V11

---
+++ src/main/java/com/example/exemplev11/Exemplev11Application.java
@@ -0,0 +1,13 @@
+package com.example.exemplev11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exemplev11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exemplev11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exemplev11/Exemplev11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exemplev11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exemplev11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
